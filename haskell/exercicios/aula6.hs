ec8be2b1d399373705c0b63e1e0213897ffe7c15
Luciano Gigantelli Zago <ra182835@students.ic.unicamp.br>
2018-09-01 20:47:41 -0300

    Lesson 6 exercises

---
+++ haskell/exercicios/aula6.hs
@@ -0,0 +1,74 @@
+-- reimplemente os exercicios da aula 1 usando as funcoes de alto nivel
+-- (map, filter, fold)
+
+-- tamanho lista
+tam lista = foldr (\ _ x -> 1+x) 0 lista
+
+-- soma elementos lista
+soma_lista lista = foldl (+) 0 lista
+
+-- soma elementos pares lista
+soma_par lista = foldl (+) 0 (filter (\ x -> x `mod` 2 == 0) lista)
+
+-- soma elementos posicoes pares ?
+
+-- existe item lista
+existe item lista = foldr (\ x bool -> (x==item) || bool) False lista
+
+-- posicao item lista ?
+
+-- conta repeticao item
+repete item lista = foldr (\ _ x -> 1+x) 0 (filter (\ x -> x==item) lista)
+
+-- maior elemento lista ?
+
+-- reverte lista
+reverte lista = foldr (\ a b -> b ++ [a]) [] lista
+
+-- intercala lista 1 ?
+
+-- intercala lista 2 ?
+
+-- lista jah ordenada ?
+
+-- gera lista 1 a n ?
+
+-- ultimo elemento lista ?
+
+-- lista sem ultimo ?
+
+-- shift right ?
+
+-- shift right n vezes ?
+
+-- shift left ?
+
+-- shift left n vezes ?
+
+-- remove item 1 vez (removendo do final)
+remove1 item lista = remove1' (foldr (\ x (removeu, acc) -> if ((x==item) && removeu) then (False, acc) else (removeu, x:acc)) (True,[]) lista)
+  where remove1' (_,x) = x
+
+-- remove item toda vez
+remove item lista = filter (\ x -> x/=item) lista
+
+-- remove item lista n vezes (removendo do final)
+remove_n item n lista = remove_n' (foldr (\ x (n, acc) -> if ((x==item) && n/=0) then ((n-1), acc) else (n, x:acc)) (n,[]) lista)
+  where remove_n' (_,x) = x
+
+-- remove item ultima vez aparece
+remove_ult item lista = remove_ult' (foldr (\ x (removeu, acc) -> if ((x==item) && removeu) then (False, acc) else (removeu, x:acc)) (True,[]) lista)
+  where remove_ult' (_,x) = x
+
+-- troca velho por novo 1 vez ?
+
+-- troca velho por novo
+troca velho novo lista = map (\ x -> if x==velho then novo else x) lista
+
+-- troca velho por novo n vezes ?
+
+-- transposta da matriz formada com listas de linhas
+-- transposta
+
+-- multiplica 2 matrizes
+-- matmul
